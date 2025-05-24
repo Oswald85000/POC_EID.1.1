740855a3ce0d4cfdcf1ca74d7768138c2ec90cbd
Oswald85000 <oswaldvandaele@hotmail.fr>
2025-05-24 09:07:48 +0200

    Package EID Reflex core

---
+++ eid_reflex_core/src/eid_model.c
@@ -0,0 +1,9 @@
+#include "libeid.h"
+
+static inline float eid_update(eid_ctx_t *c,float H,float F,float O,bool *alarm){
+    float dHdt = -c->alpha*H + c->beta*F - c->gamma*O + c->delta;
+    *alarm = ((c->alpha/c->beta) < 1.0f);
+    return dHdt;
+}
+
+/* TODO: valider précision µ sur 1 Ulp */
